c020654dd241b9d0b59d7bc6bea4eb374152800e
Mathieu Santostefano <msantostefano@jolicode.com>
2021-03-04 23:27:58 +0100

    Remove translation.provider_factory.abstract definition

--- src/Symfony/Bundle/FrameworkBundle/Resources/config/translation_providers.php
+++ src/Symfony/Bundle/FrameworkBundle/Resources/config/translation_providers.php
@@ -15,53 +15,53 @@ use Symfony\Component\Translation\Bridge\Crowdin\Provider\CrowdinProviderFactory
 use Symfony\Component\Translation\Bridge\Loco\Provider\LocoProviderFactory;
 use Symfony\Component\Translation\Bridge\Lokalise\Provider\LokaliseProviderFactory;
 use Symfony\Component\Translation\Bridge\PoEditor\Provider\PoEditorProviderFactory;
-use Symfony\Component\Translation\Provider\AbstractProviderFactory;
 use Symfony\Component\Translation\Provider\NullProviderFactory;
 
 return static function (ContainerConfigurator $container) {
     $container->services()
-        ->set('translation.provider_factory.abstract', AbstractProviderFactory::class)
+        ->set('translation.provider_factory.null', NullProviderFactory::class)
             ->args([
                 service('http_client')->ignoreOnInvalid(),
                 service('logger')->nullOnInvalid(),
                 param('kernel.default_locale'),
             ])
-            ->abstract()
-
-        ->set('translation.provider_factory.null', NullProviderFactory::class)
-            ->args([
-                service('translation.loader.xliff'),
-            ])
-            ->parent('translation.provider_factory.abstract')
             ->tag('translation.provider_factory')
 
         ->set('translation.provider_factory.crowdin', CrowdinProviderFactory::class)
         ->args([
+            service('http_client')->ignoreOnInvalid(),
+            service('logger')->nullOnInvalid(),
+            param('kernel.default_locale'),
             service('translation.loader.xliff'),
             service('translation.dumper.xliff'),
         ])
-        ->parent('translation.provider_factory.abstract')
         ->tag('translation.provider_factory')
 
         ->set('translation.provider_factory.loco', LocoProviderFactory::class)
             ->args([
+                service('http_client')->ignoreOnInvalid(),
+                service('logger')->nullOnInvalid(),
+                param('kernel.default_locale'),
                 service('translation.loader.xliff'),
             ])
-            ->parent('translation.provider_factory.abstract')
             ->tag('translation.provider_factory')
 
         ->set('translation.provider_factory.poeditor', PoEditorProviderFactory::class)
         ->args([
+            service('http_client')->ignoreOnInvalid(),
+            service('logger')->nullOnInvalid(),
+            param('kernel.default_locale'),
             service('translation.loader.xliff'),
         ])
-        ->parent('translation.provider_factory.abstract')
         ->tag('translation.provider_factory')
 
         ->set('translation.provider_factory.lokalise', LokaliseProviderFactory::class)
         ->args([
+            service('http_client')->ignoreOnInvalid(),
+            service('logger')->nullOnInvalid(),
+            param('kernel.default_locale'),
             service('translation.loader.xliff'),
         ])
-        ->parent('translation.provider_factory.abstract')
         ->tag('translation.provider_factory')
     ;
 };
